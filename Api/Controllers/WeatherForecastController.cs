e6ced036dfcacd9d3989f72658df166e2bf10af4
CORP-AD\w191003 <mehmet.yagci@wordline.com>
2025-10-05 14:51:49 +0300

    // asdasd

--- Api/Controllers/WeatherForecastController.cs
+++ Api/Controllers/WeatherForecastController.cs
@@ -32,7 +32,7 @@ namespace TestApi.Controllers
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
                 Summary = "$" + Summaries[rng.Next(Summaries.Length)] + "$"
-            })
+            }) // asdasd
             .ToArray();
         }
     }
