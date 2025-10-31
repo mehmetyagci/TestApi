1bdfb99c0dd3a077baa84ef04a237a440a2340e8
CORP-AD\w191003 <mehmet.yagci@wordline.com>
2025-10-31 11:31:42 +0300

    20251030_2

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -32,7 +32,7 @@ namespace TestApi.Controllers
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
                 Summary = "$" + Summaries[rng.Next(Summaries.Length)] + "#"
-            }) // asdasdsadsa 20251030
+            }) // asdasdsadsa 20251030_2
             .ToArray();
         }
     }
